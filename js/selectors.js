4c5d8e0d5af771f081c66f4a86c667918c8b0755
Franklyn Thompson <frankistone28@outlook.com>
2017-10-06 23:16:29 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -19,6 +19,6 @@ function divSelector(){
 }
 
 function firstListItem(){
-  var firstChild = $("ul#pic-list:first-child");
+  var firstChild = $(".pics pic-list:first-child");
   return firstChild;
 }
